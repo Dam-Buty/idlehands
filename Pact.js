da19e66f4fb95f03f1a6325020481d5de789c3cd
Damien BUTY <dam.buty@gmail.com>
2015-01-06 03:34:22 +0100

    ...

--- Pact.js
+++ Pact.js
@@ -1,38 +1,38 @@
 var Pact = function(value, element, attribute, unit) {
   return {
     ov: value,
-    value: value,
+    v: value,
     el: element,
     attr: attribute || "",
     unit: unit || "",
 
     r: function() {
-      var value = "";
+      var v = "";
 
       if (this.el !== undefined) {
         if (this.attr == "") {
-          this.el.innerHTML = this.value;
+          this.el.innerHTML = this.v;
         } else {
           if (this.unit != "") {
             if (this.unit == "%") {
             } else {
-              value = this.value + " " + this.unit;
+              v = this.v + " " + this.unit;
             }
             switch(this.unit) {
               case "%":
-                value = Math.max(Math.floor(this.value / this.ov * 100), 0) + this.unit;
+                v = Math.max(Math.floor(this.v / this.ov * 100), 0) + this.unit;
                 break;
               case "/1":
-                value = Math.max((this.value / this.ov), 0).toFixed(2);
+                v = Math.max((this.v / this.ov), 0).toFixed(2);
                 break;
               default:
-                value = this.value + " " + this.unit;
+                v = this.v + " " + this.unit;
             }
           } else {
-            value = this.value;
+            v = this.v;
           }
 
-          this.el.style.setProperty(this.attr, value);
+          this.el.style.setProperty(this.attr, v);
         }
       }
 
@@ -40,31 +40,31 @@ var Pact = function(value, element, attribute, unit) {
     },
 
     toString: function() {
-      return this.value;
+      return this.v;
     },
 
     plus: function(x) {
-      this.value += x;
+      this.v += x;
       this.r();
     },
 
     minus: function(x) {
-      this.value -= x;
+      this.v -= x;
       this.r();
     },
 
     set: function(x) {
-      this.value = x;
+      this.v = x;
       this.r();
     },
 
     upTo: function(x) {
-      this.value = Math.min(this.ov, this.value + x);
+      this.v = Math.min(this.ov, this.v + x);
       this.r();
     },
 
     downTo: function(x) {
-      this.value = Math.max(0, this.value - x);
+      this.v = Math.max(0, this.v - x);
       this.r();
     },
 
